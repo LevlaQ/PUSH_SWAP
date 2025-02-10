c19bb227b3e4cf30aa55ebc02137cafc28ff3f9d
gyildiz <gyildiz@MacBook-Air.local>
2025-02-10 16:35:27 +0300

    Error kısmı tamamlandı

--- error_functions.c
+++ error_functions.c
@@ -6,7 +6,7 @@
 /*   By: gyildiz <gyildiz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 18:49:04 by gyildiz           #+#    #+#             */
-/*   Updated: 2025/02/09 19:36:17 by gyildiz          ###   ########.fr       */
+/*   Updated: 2025/02/10 16:27:50 by gyildiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	error_main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		printf("Error(Less than two arguments)\n");
+		printf("Error(No input provided)\n");
 		return (0);
 	}
 	if (argc == 2) //string durumu burada geçerli
@@ -32,62 +32,54 @@ int	error_main(int argc, char **argv)
 	return (1);
 }
 
-int	error_two_args(char **argv) //Bir yerlerde sonsuz döngüye giriyor gibi
+int error_two_args(char **argv)
 {
-	char	**el;
-	
-	if(chk_if_string(argv[1])) // İkinci argüman bir string ise
-	{
-		if(chk_spaces(argv[1])) //Bu stringde boşluk kuralına uyulmuşsa
-		{
-			el = ft_split(argv[1], ' ');
-			if(chk_chrctrs(el)) //Stringde yasaklı karakterler kullanılmamışsa
-			{
-				if(!chk_integers_general(el)) //integer testleri *? ayrıca return 1 eklemem gerekir mi*?
-					return (free_strings(el), 0);
-				return (free_strings(el), 1);
-			}
-			printf("Error(There are forbidden chars)");
-			return (free_strings(el), 0);
-		}
-		printf("Error(There are multiple spaces)\n");
-		return (0);
-	}
-	if(!chk_if_string(argv[1])) //İKinci argüman bir string değil ise, bu verilmiş tek bir sayıdır
-	{
-		printf("Error(There is only one integer)\n");
-		return (0);
-	}
-	return (1);
+	char **el;
+
+	if (!chk_if_string(argv[1])) // Eğer argüman bir string değilse, tek bir sayı olarak kabul edilecek
+		return (printf("Error(There is only one integer)\n"), 0);
+	if (argv[1][0] == '\0' || !argv[1])
+		return (printf("Error(No input provided)\n"), 0);
+	if (!chk_spaces(argv[1])) // Bir string var, şimdi boşluk kuralını kontrol ediyoruz
+		return (printf("Error(There are multiple spaces)\n"), 0);
+	el = ft_split(argv[1], ' ');// Stringi boşluklara göre ayırıyoruz
+	if (!el || !el[0])  // ft_split'in başarısız veya boş bir dizi döndürme ihtimali kontrol ediliyor
+		return ( printf("Error(ft_split failed)\n"), 0);
+	if (!chk_chrctrs(el))  // Karakter kontrolü
+		return (printf("Error(Forbidden chars!)\n"), free_strings(el), 0);
+	if (!chk_integers_general(el))  // Sayıların geçerliliğini kontrol ediyoruz
+		return (free_strings(el), 0);
+	return (free_strings(el), 1);
 }
 
-int error_multi_args(char **argv) //chk_if_string birçok kez çalıştırılırken diğer fonksiyonlar bir kere çağırılsa yeter, düzeltme yapılmalı
+
+int error_multi_args(char **argv)
 {
 	int	i;
 
 	i = 0;
-	while(argv[i])
+	while (argv[i])// Tüm argümanları tek tek kontrol et
 	{
-		if(chk_if_string(argv[i]))
+		if (chk_if_string(argv[i]))// Argüman bir string formatında mı? (Bu durumda karışık syntax kullanımı hatalı olur)
 		{
 			printf("Error(Can't use mixed syntax)\n");
 			return (0);
 		}
-		if(chk_chrctrs(argv)) //Bütün argümanlarda yasaklı karakterlerin kontrolü
-		{
-			if(!chk_integers_general(argv)) //integer testleri *? ayrıca return 1 eklemem gerekir mi*?
-				return (0);
-			return (1);
-		}
-		else if(!chk_chrctrs(argv))
-		{
-			printf("Error(There are forbidden chars)");
-			return (0);
-		}
 		i++;
 	}
-	return (1);
+	argv = argv + 1; //a.out gönderilmesin diye
+	if (!chk_chrctrs(argv))// Argümandaki karakterler doğru mu? İlk hatada çıkış yap
+	{
+		printf("Error(There are forbidden chars)\n");
+		return (0);
+	}
+	if (!chk_integers_general(argv))// Tüm argümanlar karakter kontrolünden geçti, şimdi sayı kontrolü yap
+	{
+		return (0);
+    }
+	return (1);// Tüm kontroller başarılı, 1 döndür
 }
+
 
 int	chk_if_string(char *str)
 {
@@ -100,12 +92,12 @@ int	chk_if_string(char *str)
 	i = 0;
 	while (el[i])
 		i++;
-	if(i == 1) // Eğer ft_split sonucunda tek bir eleman elde edilmişse argüman strinf değildir
+	if(i == 1) // Eğer ft_split sonucunda tek bir eleman elde edilmişse argüman string değildir
 	{
-		free (el);
+		free_strings(el);
 		return (0);
 	}
-	free (el);
+	free_strings(el);
 	return (1); //String ise bir döndürür
 }
 
--- error_helpers.c
+++ error_helpers.c
@@ -6,7 +6,7 @@
 /*   By: gyildiz <gyildiz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 18:49:23 by gyildiz           #+#    #+#             */
-/*   Updated: 2025/02/09 19:26:21 by gyildiz          ###   ########.fr       */
+/*   Updated: 2025/02/10 16:29:16 by gyildiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,20 @@ int	chk_chrctrs(char **el)
 	int		i;
 	int		j;
 	
-	if (!el)
-		return (0); //el oluşmuş oluşmamış kontrolü
 	i = 0;
 	j = 0;
 	while (el[i])
 	{
+		j = 0;
 		while(el[i][j])
 		{
 			if((el[i][j] != '-') && !ft_isdigit(el[i][j])) // - ve rakam dışında bir şey var mı kontrolü
 				return (0);
-			else if((j != 0 && el[i][j] == '-') || (j == 0 && el[i][j + 1] != 0)) // 32- ve 003 gibi sayıların kontrolü
+			else if (el[i][0] == '-' && el[i][1] == '\0')
 				return (0);
+			else if((j != 0 && el[i][j] == '-') ||
+				(el[i][0] == '0' && el[i][1] != '\0')) // 32- ve 003 gibi sayıların kontrolü
+					return (0);
 			j++;
 		}
 		i++;
@@ -38,17 +40,17 @@ int	chk_chrctrs(char **el)
 
 int	chk_integers_general(char **el)
 {
-	if (!el)
-		return (0);
-	if (chk_minmax(el))
+	if (!chk_minmax(el))
 	{
-		if(chk_if_repeats(el))
-			return (1);
+		printf("Error(Integer overflow)\n");
+		return (0);
+	}
+	if(!chk_if_repeats(el))
+	{
 		printf("Error(Integer repeats)\n");
 		return (0);
 	}
-	printf("Error(Integer overflow)\n");
-	return (0);
+	return (1);
 }
 
 int	chk_minmax(char **el)
@@ -60,6 +62,7 @@ int	chk_minmax(char **el)
 	{
 		if(!modified_atoi_limits(el[i])) //MAX 2147483647 MIN –2147483648
 			return (0);
+		i++;
 	}
 	return (1);
 }
@@ -104,7 +107,7 @@ int	chk_if_repeats(char **el)
 		j = 0; //Bütün sayılar tek tek baştan sona diğer sayılar ile karşılaştırılacak
 		while(el[j])
 		{
-			if(ft_atoi(el[i]) == ft_atoi(el[j]))
+			if(i != j && ft_atoi(el[i]) == ft_atoi(el[j])) //Sayı kendisi ile karşılaştırılırken hata vermemeli
 				return (0);
 			j++;
 		}
